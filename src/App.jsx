4450cdac767c3019fbb99f6fdb0f1f2bec6a845e
andrewtran28 <tran.andrew@outlook.com>
2024-11-24 19:00:53 -0500

    Stylize header and store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-
 import logo from './assets/logo.png';
 import cartIcon from './assets/shopping-cart.svg';
 import Homepage from './components/Homepage';
