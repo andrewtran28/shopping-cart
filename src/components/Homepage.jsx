c62df6ea1c00ed755ac61356fa06aa56b57e02ae
andrewtran28 <tran.andrew@outlook.com>
2024-11-26 17:08:27 -0500

    fix: homepage FAQ typo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -17,10 +17,10 @@ function Homepage() {
           <h2>Frequently asked questions (FAQ)</h2>
           <p>
             <span><strong>Is this a real e-commerce site that sells goods?</strong></span><br />
-            <span>No. This site site uses the <a href="https://fakestoreapi.com/">Fake Store API</a> to display data that mimics an online web-store. No credentials are being exchanged for any real products. </span> <br /> <br/>
+            <span>No. This site uses the <a href="https://fakestoreapi.com/">Fake Store API</a> to display data that mimics an online web-store. No credentials are being exchanged for any real products. </span> <br /> <br/>
 
             <span><strong>Is there a limit to my order?</strong></span><br />
-            <span>All products can be ordered but with limited max quantity of 99 for each product.</span> <br /> <br/>
+            <span>All products can be ordered but with a limited max quantity of 99 for each product.</span> <br /> <br/>
 
             <span><strong>Is there international shipping? Are there any shipping costs?</strong></span><br />
             <span>We ship all items worldwide. We charge a shipping fee of CAD$2.99 for each product. </span> <br /> <br/>
